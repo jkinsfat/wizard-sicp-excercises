9a861635e0bfe9cda8487ce5a23fd1955b3cfc5a
jkinsfat <jasonrkinsfather@gmail.com>
2017-11-07 14:50:49 -0800

    2.27 Write iterative and recursive function to reverse embedded lists

--- chap2-dataabstractions/2.27-deep-reverse.rkt
+++ chap2-dataabstractions/2.27-deep-reverse.rkt
@@ -14,4 +14,32 @@
 ; (deep-reverse x)
 ; ((4 3) (2 1))
 
-(define (deep-reverse list))
+;; Old reverse function for reference... 
+; (define (reverse li)
+;   (define (rev-iter li result)
+;     (if (null? li)
+;       result
+;       (rev-iter (cdr li) (append (list (car li)) result))))
+;   (rev-iter li '() ))
+  
+(define (deep-reverse li)
+  (define (deep-rev-iter li result)
+    (cond ((null? li) result)
+          ((pair? (car li)) 
+            (deep-rev-iter (cdr li)
+                           (append
+                              (list (deep-rev-iter (car li) null))
+                              result)))
+          (else (deep-rev-iter (cdr li) (append (list (car li)) result)))))
+    (deep-rev-iter li null))
+
+
+(define (deep-reverse-r li) 
+  (cond ((null? li) null)
+        ((pair? (car li)) 
+         (append 
+           (deep-reverse-r (cdr li))
+           (list (deep-reverse-r (car li))))
+        ((else (append 
+          (deep-reverse-r (cdr li)) 
+          (list (car li)))))))
