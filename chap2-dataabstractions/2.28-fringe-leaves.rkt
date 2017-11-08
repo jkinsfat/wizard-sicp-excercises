4d46f40f58240265e4dff2d9f69ed26871604e69
jkinsfat <jasonrkinsfather@gmail.com>
2017-11-08 15:49:22 -0800

    2.28 Write fringe procedure that prints out all of the leaf nodes of a tree structure in-order

---
+++ chap2-dataabstractions/2.28-fringe-leaves.rkt
@@ -0,0 +1,18 @@
+; Exercise 2.28.  Write a procedure fringe that takes as argument a tree
+;  (represented as a list) and returns a list whose elements are all the
+;     leaves of the tree arranged in left-to-right order. For example,
+
+; (define x (list (list 1 2) (list 3 4)))
+
+; (fringe x)
+; (1 2 3 4)
+
+fringe (list x x))
+; (1 2 3 4 1 2 3 4)
+
+(define (fringe tree) 
+  (if ((null? tree) tree)
+      (if (not (pair? (car tree)))
+          (cons (car tree) (fringe (cdr tree)))
+          (append (fringe (car tree))
+                  (fringe (cdr tree))))))
