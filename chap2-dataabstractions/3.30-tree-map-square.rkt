fede561270dc2705849917a6dc17568773a86d2c
jkinsfat <jasonrkinsfather@gmail.com>
2017-11-08 19:52:34 -0800

    Write procedure that squares values in a tree both directly and as a higher-order function utilizing map 2.30

---
+++ chap2-dataabstractions/3.30-tree-map-square.rkt
@@ -0,0 +1,28 @@
+; Exercise 2.30.  Define a procedure square-tree analogous to the square-list procedure of exercise 2.21.
+;  That is, square-list should behave as follows:
+
+; (square-tree
+;  (list 1
+;        (list 2 (list 3 4) 5)
+;        (list 6 7)))
+; (1 (4 (9 16) 25) (36 49))
+
+; Define square-tree both directly (i.e., without using any higher-order procedures) and also by using map and recursion.
+
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square-tree (car tree))
+              (square-tree (cdr tree))))))
+
+(define (map proc seq)
+  (if (null? seq) 
+      '()
+       (cons (proc (car seq)) (map proc (cdr seq)))))
+
+(define (square-tree tree)
+  (map (lambda (x)
+        (if (not (pair? x))
+          (* x x)
+          (square-tree x)))
+        tree))
