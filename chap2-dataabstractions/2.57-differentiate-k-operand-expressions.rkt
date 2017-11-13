c50fb1a052757cd867bd69e2586bf5043dc576ea
jkinsfat <jasonrkinsfather@gmail.com>
2017-11-13 15:09:07 -0800

    begin 2.57. Edit 2.56: Account for derivation of exponential expression when of type (expt constant variable)

--- chap2-dataabstractions/2.57-differentiate-k-operand-expressions.rkt
+++ chap2-dataabstractions/2.57-differentiate-k-operand-expressions.rkt
@@ -1,8 +1,12 @@
-; Exercise 2.56.  Show how to extend the basic differentiator to handle more kinds of expressions. 
-; For instance, implement the differentiation rule of u raised to a power n by adding a new clause
-; to the deriv program and defining appropriate procedures exponentiation?, base, exponent, and 
-; make-exponentiation. (You may use the symbol ** to denote exponentiation.) Build in the rules 
-; that anything raised to the power 0 is 1 and anything raised to the power 1 is the thing itself.
+; Exercise 2.57.  Extend the differentiation program to handle sums and products of arbitrary numbers 
+; of (two or more) terms. Then the last example above could be expressed as
+
+; (deriv '(* x y (+ x 3)) 'x)
+
+; Try to do this by changing only the representation for sums and products, without changing the deriv
+;  procedure at all. For example, the aiddend of a sum would be the first term, and the augend would be 
+;  the sum of the rest of the terms.
+
 (define (variable? exp) (symbol? exp))
 (define (same-variable? v1 v2) (eq? v1 v2))
 (define (equal-number? value num) (and (number? n) (= value num)))
@@ -28,18 +32,6 @@
 (define (multiplier product) (cadr product))
 (define (multiplicand product) (caddr product))
 
-;;**********************
-(define (make-exponent b e)
-  (cond ((=number? e 0) 1)
-        ((=number? e 1) b)
-        ((and (number? b) (number? e)) (expt b e))
-        (else (list 'expt base exponent))))
-
-(define (exponentiation? exp) (and (pair? exp) (eq? (car exp) 'expt)))
-(define (base exponentiation) (cadr exponentiation))
-(define (exponent exponentiation) (caddr exponentiaton))
-;;**********************
-
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp)
@@ -53,13 +45,6 @@
                          (deriv (multiplicand exp) var))
            (make-product (deriv (multiplier exp) var)
                          (multiplicand exp))))
-        ((exponentiation? exp)
-         (if (same-variable? (exponent exp) var)
-             exp
-             (make-product (base exp) 
-                           (make-exponent (base exp) 
-                                          (make-sum (exponent exp)
-                                                    (- 1))))))
         (else
          (error "unknown expression type -- DERIV" exp))))
 
